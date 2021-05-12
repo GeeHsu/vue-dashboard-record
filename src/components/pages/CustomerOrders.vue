8c36748637eba2eee952ac94a12d74d2596ea881
Gee Hsu <james81429@yahoo.com.tw>
2021-05-12 09:59:00 +0800

    add shop cart list

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -75,6 +75,54 @@
         </div>
       </div>
     </div>
+    <!-- 購物車列表 -->
+    <div class="my-5 row justify-content-center">
+      <div class="col-md-6">
+        <table class="table">
+          <thead>
+            <th></th>
+            <th>品名</th>
+            <th>數量</th>
+            <th>單價</th>
+          </thead>
+          <tbody>
+            <tr v-for="item in cart.carts" :key="item.id" v-if="cart.carts">
+              <td class="align-middle">
+                <button type="button" class="btn btn-outline-danger btn-sm">
+                  <i class="far fa-trash-alt"></i>
+                </button>
+              </td>
+              <td class="align-middle">
+                {{ item.product.title }}
+                <!-- <div class="text-success" v-if="item.coupon">
+                  已套用優惠券
+                </div> -->
+              </td>
+              <td class="align-middle">{{ item.qty }}/{{ item.product.unit }}</td>
+              <td class="align-middle text-right">{{ item.final_total }}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="3" class="text-right">總計</td>
+              <td class="text-right">{{ cart.total }}</td>
+            </tr>
+            <!-- <tr>
+              <td colspan="3" class="text-right text-success">折扣價</td>
+              <td class="text-right text-success">{{ cart.final_total }}</td>
+            </tr> -->
+          </tfoot>
+        </table>
+        <!-- <div class="input-group mb-3 input-group-sm">
+          <input type="text" class="form-control" placeholder="請輸入優惠碼">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">
+              套用優惠碼
+            </button>
+          </div>
+        </div> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -89,6 +137,7 @@ export default {
       status: {
         loadingItem: '',
       },
+      cart: {},
       isLoading: false,
     };
   },
@@ -143,6 +192,7 @@ export default {
       vm.isLoading = true;
       this.$http.get(url).then((response) => {
         // vm.products = response.data.products;
+        vm.cart = response.data.data;
         console.log(response);
         vm.isLoading = false;
       });
